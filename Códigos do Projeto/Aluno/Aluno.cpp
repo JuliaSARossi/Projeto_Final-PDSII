c1f52e5010afd8b8c0d7cb397f1e7fff8ce4bfaa
LucasAraujoUFMG <148714205+LucasAraujoUFMG@users.noreply.github.com>
2023-11-27 21:41:17 -0300

    Update Aluno.cpp

--- Códigos do Projeto/Aluno/Aluno.cpp
+++ Códigos do Projeto/Aluno/Aluno.cpp
@@ -1,5 +1,3 @@
-// aluno.cpp
-
 #include "Aluno.hpp"
 
 Aluno::Aluno() : nome(""), sobrenome(""), email(""), idade(0), sexo(' '), login(""), senha("") {}
@@ -7,7 +5,6 @@ Aluno::Aluno() : nome(""), sobrenome(""), email(""), idade(0), sexo(' '), login(
 Aluno::Aluno(const std::string& nome, const std::string& sobrenome, const std::string& email, int idade, char sexo, const std::string& login, const std::string& senha)
     : nome(nome), sobrenome(sobrenome), email(email), idade(idade), sexo(sexo), login(login), senha(senha) {}
 
-// Implementação dos métodos de acesso
 std::string Aluno::getNome() const { return nome; }
 std::string Aluno::getSobrenome() const { return sobrenome; }
 std::string Aluno::getEmail() const { return email; }
